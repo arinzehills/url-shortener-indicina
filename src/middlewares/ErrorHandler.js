729fd68539d744a1a8192c39d2ede0029469de58
arinzehills <arinzehill@gmail.com>
2024-06-11 18:08:05 +0100

    Adding centralized error handling middleware

---
+++ src/middlewares/ErrorHandler.js
@@ -0,0 +1,11 @@
+class ErrorHandler {
+  static handleErrors(err, req, res, next) {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+      message: err.message || "Internal Server Error",
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+  }
+}
+
+module.exports = ErrorHandler;
