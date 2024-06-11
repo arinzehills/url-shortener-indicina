c368a1033ced4678dcf2444c8f118bd6949a256b
arinzehills <arinzehill@gmail.com>
2024-06-11 19:13:07 +0100

    added ded validation for decoding url

--- src/middlewares/ValidationMiddleware.js
+++ src/middlewares/ValidationMiddleware.js
@@ -12,6 +12,23 @@ class ValidationMiddleware {
     }
     next();
   }
+  static validateShortUrl(req, res, next) {
+    const { shortUrl } = req.body;
+
+    if (!shortUrl) {
+      return res
+        .status(400)
+        .json({ message: "Short URL to decode is required" });
+    }
+    const id = shortUrl.split("/").pop();
+    console.log("ID", id);
+    if (!id || !validator.isURL(shortUrl)) {
+      // if () {
+      return res.status(400).json({ message: "Invalid URL" });
+    }
+
+    next();
+  }
 }
 
 module.exports = ValidationMiddleware;
--- src/routes/urlRoutes.route.js
+++ src/routes/urlRoutes.route.js
@@ -4,7 +4,11 @@ const ValidationMiddleware = require("../middlewares/ValidationMiddleware");
 const router = express.Router();
 
 router.post("/encode", ValidationMiddleware.validateUrl, UrlController.encode);
-router.post("/decode", UrlController.decode);
+router.post(
+  "/decode",
+  ValidationMiddleware.validateShortUrl,
+  UrlController.decode
+);
 // router.get('/statistic/:urlPath', UrlController.getStatistic);
 
 module.exports = router;
