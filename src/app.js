653e59d59efad359572ce2248e3143eddd6122d7
arinzehills <arinzehill@gmail.com>
2024-06-11 18:09:22 +0100

    Adding centralized error handling middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use(ErrorHandler.handleErrors);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port:: http://localhost:${PORT}/`);
