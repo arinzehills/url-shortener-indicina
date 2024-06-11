b689fdeb4273a20dbe5825433888348942be9325
arinzehills <arinzehill@gmail.com>
2024-06-11 18:01:34 +0100

    Setting up test cases for my endpoint

---
+++ src/tests/url.test.js
@@ -0,0 +1,5 @@
+describe("URL Shortener API", () => {
+  it("should encode a URL", async () => {});
+  it("should decode a URL", async () => {});
+  it("should return statistics for a URL", async () => {});
+});
