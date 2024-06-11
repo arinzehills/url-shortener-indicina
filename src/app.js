1a4227886ed965167cf1baa8f6c10e4b83c20559
arinzehills <arinzehill@gmail.com>
2024-06-11 18:25:43 +0100

    setting up url routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const urlRoutes = require("./routes/urlRoutes.route");
+const ErrorHandler = require("./middlewares/ErrorHandler");
 
 app.use(express.json());
+app.use("/api", urlRoutes);
 
 app.use(ErrorHandler.handleErrors);
 const PORT = process.env.PORT || 3000;
--- src/routes/urlRoutes.route.js
+++ src/routes/urlRoutes.route.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const UrlController = require("../controllers/UrlController");
 const router = express.Router();
 
-router.post("/encode", async (req, res) => {});
-router.post("/decode", async (req, res) => {});
-router.get("/statistic/:urlPath", async (req, res) => {});
+router.post("/encode", async (req, res) => {
+  res.sendStatus(200);
+});
+router.post("/decode", async (req, res) => {
+  res.sendStatus(200);
+});
+router.get("/statistic/:urlPath", async (req, res) => {
+  console.log(req.params);
+  res.sendStatus(200);
+});
 
 module.exports = router;
