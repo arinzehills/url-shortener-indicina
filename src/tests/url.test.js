755c32a59e5f95cfb70c8c6d1db4b844e1cc39d0
arinzehills <arinzehill@gmail.com>
2024-06-11 18:02:53 +0100

    Implementing test case for encode endpoint

--- src/tests/url.test.js
+++ src/tests/url.test.js
@@ -1,5 +1,14 @@
 describe("URL Shortener API", () => {
-  it("should encode a URL", async () => {});
+  it("should encode a URL", async () => {
+    const res = await request(app)
+      .post("/api/encode")
+      .send({ longUrl: "https://indicina.co" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("shortUrl");
+    expect(resDecode.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+  });
   it("should decode a URL", async () => {});
   it("should return statistics for a URL", async () => {});
 });
