af5e7d5e5d7f6157e5ca6d6a278681610184c4a1
arinzehills <arinzehill@gmail.com>
2024-06-11 18:55:32 +0100

    Added validation to check if the url is a correct url

--- src/routes/urlRoutes.route.js
+++ src/routes/urlRoutes.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const UrlController = require("../controllers/UrlController");
+const ValidationMiddleware = require("../middlewares/ValidationMiddleware");
 const router = express.Router();
 
-router.post("/encode", UrlController.encode);
+router.post("/encode", ValidationMiddleware.validateUrl, UrlController.encode);
 // router.post('/decode', UrlController.decode);
 // router.get('/statistic/:urlPath', UrlController.getStatistic);
 
